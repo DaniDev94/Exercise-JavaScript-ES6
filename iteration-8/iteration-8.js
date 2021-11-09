47451837e8e60e954a1f7ad3c420cb6ff95d9143
Daniel López <daniel.lp1094@gmail.com>
2021-11-09 20:25:37 +0100

    FEAT: iteration#8

---
+++ iteration-8/iteration-8.js
@@ -0,0 +1,27 @@
+// 8.1-------------------------------------------------->
+const videogames = [
+    {name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5},
+    {name: 'Assasins Creed Valhala', genders: ['Aventura', 'RPG'], score: 4.5},
+    {name: 'The last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8},
+    {name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5},
+    {name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5},
+    {name: 'Legend of Zelda: Breath of the wild', genders: ['RPG', 'La cosa más puto bonita que he visto nunca'], score: 10},
+]
+
+const filterGame = videogames.filter(videogame =>{
+    return videogame.genders.includes('RPG');
+})
+
+const copyResults = [...filterGame];
+console.log(copyResults)
+
+const totalAverage = copyResults.reduce((acc, cur) => {
+    return acc + cur.score
+},0);
+
+console.log(totalAverage);
+
+
+
+
+
